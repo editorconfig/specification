47c7bea7cf27219695d8256ded89d0c2a4e27b30
Chris White <cxwembedded@gmail.com>
2024-09-15 22:23:59 -0400

    spelling_language clarification: ss, TT are two-character codes (#54)
    We do not support the ISO numeric or three-letter codes.
    
    Fixes #48

--- conf.py
+++ conf.py
@@ -21,8 +21,8 @@ project = 'EditorConfig Specification'
 copyright = '2019--2024, EditorConfig Team'
 author = 'EditorConfig Team'
 
-version = '0.17.0'
-release = '0.17.0'
+version = '0.17.1'
+release = '0.17.1'
 
 # -- General configuration ---------------------------------------------------
 
